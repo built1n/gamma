291bfc71ae719e442993daf30548ef3bd46aafcd
Franklin Wei <root@localhost>
1998-12-31 20:19:32 -0500

    Rearrange code

--- kernel_main.c
+++ kernel_main.c
@@ -7,17 +7,21 @@ extern "C"
 #endif
 void gpf(registers_t regs)
 {
+  term_puts("Recieved general protection failure!\n");
 }
 int kernel_main(struct multiboot *mboot_ptr)
 {
   init_desc_tables();
   init_clock(10); // do this ASAP, argument is time in milliseconds
-  register_handler(0x0D, &gpf);
   init_terminal();
   term_puts("terminal initialized.\n");
   term_puts("kernel booting...\n");
-  set_unhandled_panic(false); // we've already registered all handlers
+  // register interrupt handlers
+  register_handler(0x0D, &gpf);
+  set_unhandled_panic(true); // we've already registered all handlers
+
  sys_idle:
   goto sys_idle; // let the system run
+
   return 0xDEADBEEF; // we should never get here
 }
