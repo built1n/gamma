6b5f7c23d14f48d5ac86696fbdf9bdcbe06876af
Franklin Wei <root@localhost>
1998-12-31 19:07:17 -0500

    Added GPF handler

--- kernel_main.c
+++ kernel_main.c
@@ -5,19 +5,19 @@
 #ifdef __cplusplus
 extern "C"
 #endif
+void general_protection_fault(int err)
+{
+  term_puts("General protection fault!\n");
+}
 int kernel_main(struct multiboot *mboot_ptr)
 {
   init_terminal();
   term_puts("terminal initialized.\n");
   init_desc_tables();
+  register_handler(0xD, &general_protection_fault);
   term_puts("kernel booting...\n");
   // boot
   
   // do stuff
-  term_puts("42 in hex is: ");
-  term_putn_hex(42);
-  term_puts("\nIn binary: ");
-  term_putn_bin(42);
-  term_puts("\nenabling interrupts...\n");
   return 0xDEADBEEF;
 }
