7ad6224bc20f49992ecaa7da56b7b17b50bce6eb
Franklin Wei <root@localhost>
1998-12-31 19:02:39 -0500

    Added GDT, untested

---
+++ gdt.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include <stdint.h>
+extern void flush_gdt(uint32_t); // asm function
+gdt_entry_type gdt_entries[5];
+gdt_ptr gdt_pointer;
+idt_entry_type idt_entries[256];
+static void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t granularity)
+{
+  gdt_entries[num].base_low=(base & 0xFFFF);
+  gdt_entries[num].base_middle=(base >> 16) & 0xFF;
+  gdt_entries[num].base_high=(base >> 24) & 0xFF;
+  gdt_entries[num].limit_low=(limit & 0xFFFF);
+  gdt_entries[num].granularity=(limit >> 16) & 0x0F;
+  gdt_entries[num].granularity|=(gran&0xF0);
+  gdt_entries[num].access=access;
+}
+static void init_gdt()
+{
+  gdt_pointer.limit=(sizeof(gdt_entry_type)*5)-1;
+  gdt_pointer.base=&get_entries;
+  gdt_set_gate(0,0,0,0,0); // null seg.
+  gdt_set_gate(1,0,0xFFFFFFFF, 0x9A, 0xCF);
+  gdt_set_gate(2,0,0xFFFFFFFF, 0x92, 0xCF);
+  gdt_set_gate(3,0,0xFFFFFFFF, 0xFA, 0xCF);
+  gdt_set_gate(4,0,0xFFFFFFFF, 0xF2, 0xCF);
+  gdt_flush((uint32_t)&gdt_pointer);
+}
--- gdt.h
+++ gdt.h
@@ -1,2 +1,15 @@
-struct gdt_entry_type {
-  
+#include <stddef.h>
+#include <stdint.h>
+struct gdt_entry_type { // do not alter the order of these!
+  uint16_t limit_low;
+  uint16_t base_low;
+  uint8_t base_middle;
+  uint8_t access;
+  uint8_t granularity;
+  uint8_t base_high;
+}; __attribute__((packed)); // tell GCC to keep the contents in the order given
+struct gdt_ptr {
+  uint16_t limit;
+  uint32_t base;
+} __attribute__((packed)); // same purpose as above
+void init_desc_tables(void);
