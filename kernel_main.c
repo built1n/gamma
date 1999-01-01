234550087d4c5a5134b81320063fa37f427872ef
Franklin Wei <root@localhost>
1998-12-31 19:05:46 -0500

    Initialize clock

--- kernel_main.c
+++ kernel_main.c
@@ -5,15 +5,19 @@
 #ifdef __cplusplus
 extern "C"
 #endif
+void gpf(registers_t regs)
+{
+}
 int kernel_main(struct multiboot *mboot_ptr)
 {
   init_desc_tables();
-  register_handler(8, &clock_tick);
-  set_unhandled_panic(false); // we've already registered all handlers
+  init_clock(10); // do this ASAP, argument is time in milliseconds
+  register_handler(0x0D, &gpf);
   init_terminal();
   term_puts("terminal initialized.\n");
   term_puts("kernel booting...\n");
- idle:
-  goto idle; // let the system run
+  set_unhandled_panic(false); // we've already registered all handlers
+ sys_idle:
+  goto sys_idle; // let the system run
   return 0xDEADBEEF; // we should never get here
 }
