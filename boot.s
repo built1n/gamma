4ede3fbfff4efcad84ea30fc4e65d77f9623c972
Franklin Wei <root@localhost>
1998-12-31 19:06:08 -0500

    Cleaned up code

--- boot.s
+++ boot.s
@@ -1,18 +1,21 @@
 	.set ALIGN,	 1<<0 	# Multiboot stuff
 	.set MEMINFO,	 1<<1
 	.set FLAGS,	 ALIGN | MEMINFO
-	.set MAGIC,	 0x1BADB002
+	.set MAGIC,	 0x1BADB002 # 1 Bad Boot
 	.set CHECKSUM,	 -(MAGIC + FLAGS)
-	.section .multiboot
+	
+.section .multiboot
 	.align 4
 	.long MAGIC
 	.long FLAGS
 	.long CHECKSUM		# Prove that we are multiboot
-	.section .initialize_stack
-stack_bottom:			# Grows downwards
+	
+.section .initialize_stack
+stack_bottom:			# Grows downwards in memory
 	.skip 16384		# 16K
 stack_top:
-	.section .text
+	
+.section .text
 	.global _start
 	.type _start, @function
 _start:
