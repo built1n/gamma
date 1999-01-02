e342e691ba649348dadf2d728ab284e2b24cfcd2
Franklin Wei <root@localhost>
1999-01-01 19:07:31 -0500

    Change layout, does not work yet

--- page.c
+++ page.c
@@ -1,28 +1,11 @@
 #include "gamma.h"
 #include <stdint.h>
 page_dir_t* current_directory;
-void init_paging(void)
-{
-  uint32_t mem_end_page=0x1000000; // 16MB for now
-  nFrames=mem_end_page/0x1000;
-  frames=(uint32_t*)kmalloc(INDEX_FROM_BIT(nFrames));
-  memset(frames, 0, INDEX_FROM_BIT(nFrames));
-  page_dir_t* kernel_dir=(page_dir_t*)kmalloc_a(sizeof(page_dir_t));
-  memset(kernel_dir, 0, sizeof(page_dir_t));
-  current_directory=kernel_dir;
-  int i=0;
-  while(i<next_addr)
-    {
-      alloc_frame( get_page(i, 1, kernel_dir),0,0);
-      i+=0x1000;
-    }
-  register_handler(14, &page_fault);
-  switch_page_directory(kernel_dir);
-}
+
 void switch_page_directory(page_dir_t* page_dir)
 {
-  current_directory=dir;
-  asm volatile("mov %0, %%cr3": : "r"(&dir->page_tables_phys));
+  current_directory=page_dir;
+  asm volatile("mov %0, %%cr3": : "r"(&page_dir->page_tables_phys));
   uint32_t cr0=0; // shut up GCC
   asm volatile("mov %%cr0, %0": "=r"(cr0));
   cr0 |=0x80000000;
@@ -50,7 +33,7 @@ page_t *get_page(uint32_t addr, int make, page_dir_t* dir)
 void page_fault(registers_t regs)
 {
   uint32_t faulting_addr;
-  asm volatile("mov %%cr2, %0", : "=r" (faulting_addr));
+  asm volatile("mov %%cr2, %0" : "=r" (faulting_addr));
   int present= !(regs.error_code & 1);
   int rw=(regs.error_code & 2);
   int us=(regs.error_code & 4);
@@ -60,4 +43,22 @@ void page_fault(registers_t regs)
   term_putn_hex(faulting_addr);
   term_puts("\n");
   panic("page fault");
+}
+void init_paging(void)
+{
+  uint32_t mem_end_page=0x1000000; // 16MB for now
+  nFrames=mem_end_page/0x1000;
+  frames=(uint32_t*)kmalloc(INDEX_FROM_BIT(nFrames));
+  memset(frames, 0, INDEX_FROM_BIT(nFrames));
+  page_dir_t* kernel_dir=(page_dir_t*)kmalloc_a(sizeof(page_dir_t));
+  memset(kernel_dir, 0, sizeof(page_dir_t));
+  current_directory=kernel_dir;
+  int i=0;
+  while(i<next_addr)
+    {
+      alloc_frame( get_page(i, 1, kernel_dir),0,0);
+      i+=0x1000;
+    }
+  register_handler(14, &page_fault);
+  switch_page_directory(kernel_dir);
 }
