9a37d03d25f53e3a0cb6994eb286f9a46c744aa4
Franklin Wei <root@localhost>
1999-01-01 19:05:52 -0500

    Change layout

--- kernel_main.c
+++ kernel_main.c
@@ -4,25 +4,40 @@
 #include "gamma.h"
 void gpf(registers_t regs)
 {
-  term_puts("Recieved general protection failure!\n");
+  term_puts("General protection fault\n");
 }
-#ifdef __cplusplus
-extern "C"
-#endif
-int kernel_main(struct multiboot *mboot_ptr)
+void dividebyzero(registers_t regs)
+{
+  panic("Divide by zero");
+}
+static void early_init(void)
 {
   init_desc_tables();
   init_terminal();
-  init_clock(100); // do this AFTER terminal initialization
   term_puts("terminal initialized.\n");
-  term_puts("kernel booting...\n");
+  init_clock(100); // do this AFTER terminal initialization 
+  term_puts("clock initialized.\n");
+}
+static void init(void)
+{
   init_ps2();
   term_puts("keyboard initialized.\n");
   // register interrupt handlers
   register_handler(0xD, &null_handler);
+  register_handler(0, &dividebyzero);
   set_unhandled_panic(true); // we've already registered all the handlers
- sys_idle:
-  goto sys_idle; // let the system run
-
-  return 0xDEADBEEF; // we should never get here
+}
+#ifdef __cplusplus
+extern "C"
+#endif
+int kernel_main(struct multiboot *mboot_ptr) // kernel entry point
+{
+  early_init();
+  term_puts("booting...\n");
+  init();
+  asm volatile("sti"); // enable interrupts
+  term_puts("system initialized.\n");
+ sys_run:
+  goto sys_run; // let the system run
+  return 0xBADC0DE; // we should never get here
 }
