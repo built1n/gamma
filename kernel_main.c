1aab5c7e1e88139efb5ca9ffe7209751711873ad
Franklin Wei <root@localhost>
1999-01-03 07:06:10 -0500

    Fixed cli crash

--- kernel_main.c
+++ kernel_main.c
@@ -11,12 +11,13 @@ int kernel_main(struct multiboot *mboot_ptr)
   term_puts("terminal initialized.\n");
   init_desc_tables();
   term_puts("kernel booting...\n");
-  asm volatile ("cli");
+  // boot
+  
+  // do stuff
   term_puts("42 in hex is: ");
   term_putn_hex(42);
   term_puts("\nIn binary: ");
   term_putn_bin(42);
   term_puts("\nenabling interrupts...\n");
-  asm volatile ("sti");
   return 0xDEADBEEF;
 }
