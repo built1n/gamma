52df7ea63c5aa85b85ab669b360fe2bfdd5f6817
Franklin Wei <root@localhost>
2014-01-26 16:16:19 -0500

    Removed init_paging call, for now

--- kernel_main.c
+++ kernel_main.c
@@ -1,3 +1,14 @@
+/* +==============================================+
+   |                                              |
+   |  /====\   /=\   +=\   /=+  +=\   /=+   /=\   |
+   |  | +==/  //^\\  |  \ /  |  |  \ /  |  //^\\  |
+   |  | |+=+  ||_||  |   V   |  |   V   |  ||_||  |
+   |  | || |  ||-||  | |\ /| |  | |\ /| |  ||-||  |
+   |  | ++ |  || ||  | || || |  | || || |  || ||  | 
+   |  \====/  ++ ++  +=++=++=+  +=++=++=+  ++ ++  |
+   |                                              |
+   +==============================================+
+*/
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -9,17 +20,18 @@ void dividebyzero(registers_t regs)
 static void early_init(void) // do low-level init
 {
   init_desc_tables(); // GDT, IDT
+  register_handler(0xD, &null_handler); /// FIXME: int 0xD gets sent constantly
   init_terminal();
   term_puts("Terminal initialized.\n");
   init_clock(100);
   term_puts("System clock initialized.\n");
+  // init_paging(); // BUGGY!
 }
 static void init(void)
 {
   init_ps2();
   term_puts("PS/2 keyboard initialized.\n");
   // register interrupt handlers
-  register_handler(0xD, &null_handler); /// FIXME: int 13 gets sent constantly!
   register_handler(0, &dividebyzero);
   set_unhandled_panic(true); // we've already registered all the handlers
 }
@@ -33,7 +45,6 @@ int kernel_main(struct multiboot *mboot_ptr) // kernel entry point
   init();
   asm volatile("sti"); // enable interrupts
   term_puts("System initialized.\n");
-  term_puts("");
  sys_run:
   goto sys_run; // let the system run
   return 0xDEADBEEF; // we should never get here
