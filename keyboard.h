02c7567e96f83309a780060d8b15c3a37c41a44b
Franklin Wei <root@localhost>
2014-01-27 17:00:08 -0500

    New logo art

--- keyboard.h
+++ keyboard.h
@@ -1,3 +1,12 @@
+/*
+        ####       ##       ##    ##       ##    ##       ##
+       ## ##      ###      ###   ###      ###   ###      ###
+      ##  ##     ####     ####  ####     ####  ####     ####
+     ##         ## ##    ## ## ## ##    ## ## ## ##    ## ##
+    ##   ###   ######   ##  ####  ##   ##  ####  ##   ######
+   ##     ##  ##   ##  ##   ###   ##  ##   ###   ##  ##   ##
+  ########## ##    ## ##    ##    ## ##    ##    ## ##    ##
+*/
 void init_ps2();
 void register_keyboard_handler(void(*fnPtr)(char)); // register 1 function to call when a key is pressed
 void *get_keyboard_handler();
