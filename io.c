5adea8c9013d549985325b3ffe398630fc6c621c
Franklin Wei <root@localhost>
2014-01-28 16:51:30 -0500

    More read fixes, but it STILL does not work

--- io.c
+++ io.c
@@ -1,7 +1,7 @@
 #include "gamma.h"
 static char* read_buf;
 static int idx=0, maxchars;
-static volatile int readdone=0;
+volatile int readdone=0;
 void on_keypress(char c)
 {
   if(c!='\n' && c!='\b' && c!=127)
@@ -11,13 +11,19 @@ void on_keypress(char c)
 	  read_buf[idx]=c;
 	  ++idx;
 	}
+      term_puts("Buffer: ");
+      term_puts(read_buf);
+      term_puts("\n");
     }
   else
     {
       switch(c)
 	{
 	case '\n':
-	  readdone=1;
+	  readdone=1; // this does not work!
+	  term_puts("Value of readdone: ");
+	  term_putn_dec(readdone);
+	  term_putchar('\n');
 	  break;
 	case '\b': case 127:
 	  if(idx>0)
@@ -35,18 +41,27 @@ static void idle()
 }
 int read(int n, char* buf)
 {
-  term_puts("Entering read function.\n");
-  void* old_handler=get_keyboard_handler();
-  idx=0;
-  readdone=0;
-  read_buf=buf;
-  maxchars=n;
-  term_puts("Registering new keyboard handler.\n");
-  register_keyboard_handler(&on_keypress);
-  term_puts("Looping...\n");
-  while(readdone==0)
-    idle();
-  term_puts("Done!\n");
-  register_keyboard_handler(old_handler);
-  return idx;
+  if(buf)
+    {
+      memset(buf, 0, n);
+      term_puts("Entering read function.\n");
+      void* old_handler=get_keyboard_handler();
+      idx=0;
+      readdone=0;
+      read_buf=buf;
+      maxchars=n;
+      term_puts("Registering new keyboard handler.\n");
+      register_keyboard_handler(&on_keypress);
+      term_puts("Looping...\n");
+    loop: // this is neccessary, GCC will optimize a loop
+      if(readdone==0)
+	goto loop;
+      else
+	goto done;
+    done:
+      term_puts("Done!\n");
+      register_keyboard_handler(old_handler);
+      return idx;
+    }
+  return -1;
 }
