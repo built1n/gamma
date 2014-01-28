04c48673a5f72ecd1f12cd84d53121d31f7cf6ba
Franklin Wei <root@localhost>
2014-01-28 14:38:03 -0500

    Removed debug prints

--- string.c
+++ string.c
@@ -31,13 +31,11 @@ void memset(void* ptr, unsigned char value, size_t n)
 }
 int strcmp(char* str1, char* str2)
 {
-  term_puts("in strcmp.\n");
   for(int i=0;!str1[i] && !str2[i];++i)
     {
       int delta=str2[i]-str1[i];
       if(delta)
 	return delta; 
-      term_puts("strcmp loop\n");
     }
   return 0;
 }
