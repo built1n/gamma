16a4365368adc3aa10c762291b6ecb467977a563
theunamedguy <theunamedguy@vm-0.theunamedguy.koding.kd.io>
2014-01-30 19:32:54 +0000

    Revert "removed extra files" This is to restore the README This reverts commit 93f9bed5ca4b412a23b0993900cd1adb7beabea8.

---
+++ gdt-as.s
@@ -0,0 +1,23 @@
+	.global gdt_flush  # function prototype: void gdt_flush(uint32_t addr)
+	.type gdt_flush, @function
+gdt_flush:
+	cli # this is a critical section of code!
+	movl 4(%esp), %eax # load the address off the stack
+	lgdt (%eax) # load the GDT
+	mov $0x10, %ax # set all section registers to 16
+	mov %ax, %ds
+	mov %ax, %es
+	mov %ax, %fs
+	mov %ax, %gs
+	mov %ss, %ax
+	sti # enable interrupts
+	ret
+	
+	.global idt_flush # function prototype: void idt_flush(uint32_t addr)
+	.type idt_flush, @function
+idt_flush:
+	cli # this is a critical section of code!
+	movl 4(%esp), %eax # load the address off the stack
+	lidt (%eax) # load the IDT
+	sti # resume interrupts
+	ret
---
+++ gdt.h
@@ -0,0 +1,26 @@
+/*
+        ####       ##       ##    ##       ##    ##       ##
+       ## ##      ###      ###   ###      ###   ###      ###
+      ##  ##     ####     ####  ####     ####  ####     ####
+     ##         ## ##    ## ## ## ##    ## ## ## ##    ## ##
+    ##   ###   ######   ##  ####  ##   ##  ####  ##   ######
+   ##     ##  ##   ##  ##   ###   ##  ##   ###   ##  ##   ##
+  ########## ##    ## ##    ##    ## ##    ##    ## ##    ##
+*/
+#include <stddef.h>
+#include <stdint.h>
+struct gdt_entry_type { // do not alter the order of these!
+  uint16_t limit_low;
+  uint16_t base_low;
+  uint8_t base_middle;
+  uint8_t access;
+  uint8_t granularity;
+  uint8_t base_high;
+} __attribute__((packed)); // tell GCC to keep the contents in the order given
+typedef struct gdt_entry_type gdt_entry_type;
+struct gdt_ptr {
+  uint16_t limit;
+  uint32_t base;
+} __attribute__((packed)); // same purpose as above
+typedef struct gdt_ptr gdt_ptr;
+void init_desc_tables(void);
