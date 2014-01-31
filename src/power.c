656514c9aecd81b76d05da3b52c1c3e731c03c04
theunamedguy <theunamedguy@vm-0.theunamedguy.koding.kd.io>
2014-01-31 20:51:07 +0000

    Reverted to before restructure

--- src/power.c
+++
@@ -1,9 +0,0 @@
-#include <gamma.h>
-void power_off(void)
-{
-  clear_idt(); 
-  asm volatile("int $0x3"); // generate a triple-fault
-  kprintf("Shutdown failed!\n");
- l:
-  goto l; // for good measure
-}
