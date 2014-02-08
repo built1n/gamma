20a671135b0de03a00ecfcb00d72bee116d486ed
Franklin Wei <frankhwei536@gmail.com>
2014-02-08 18:46:52 -0500

    Changed variables to 32-bit

--- clock.c
+++ clock.c
@@ -9,7 +9,7 @@
 */
 #include "gamma.h"
 #include <stdint.h>
-unsigned long long tick; // hundredths of a second
+uint32_t tick=0; // hundredths of a second, will overflow in 1.3 years
 void clock_tick(registers_t regs)
 {
   ++tick;
@@ -19,18 +19,18 @@ void init_clock(uint32_t frequency)
   tick=0;
   uint32_t divisor=CLOCK_FREQ/frequency;
   byte low=(byte)(divisor & 0xFF), high=(byte)( (divisor >> 8) & 0xFF);
-  register_handler(32, &clock_tick);
+  register_handler(32, &clock_tick); // IRQ0
   outb(0x43, 0x36);
   outb(0x40, low);
   outb(0x40, high);
 }
-uint64_t time(void)
+uint32_t time(void)
 {
   return tick; 
 }
 void sleep(uint32_t n)
 {
-  int stop=tick+n;
-  while(tick!=stop)
+  uint32_t stop=tick+n;
+  while(tick<=stop)
     ;
 }
--- clock.h
+++ clock.h
@@ -12,5 +12,5 @@
 #define CLOCK_FREQ 1193180 // this is the same for all Intel CPUs
 void clock_tick(registers_t);
 void init_clock(uint32_t);
-uint64_t time(void); // in hundredths of a second
+uint32_t time(void); // in hundredths of a second
 void sleep(uint32_t n); // sleep n hundredths of a second, accurate to 1 hundredth of a second
