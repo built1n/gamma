c63fe436bbb095538ec7d912cb93249f9e19089f
Franklin Wei <frankhwei536@gmail.com>
2014-02-13 16:21:18 -0500

    Added ctrl-alt-del handler

--- keyboard.h
+++ keyboard.h
@@ -10,3 +10,5 @@
 void init_ps2();
 void register_keyboard_handler(void(*fnPtr)(char)); // register 1 function to call when a key is pressed
 void *get_keyboard_handler();
+void set_leds(int, int, int);
+void ctrlaltdel();// actually defined in kernel_main.c
