2d17878e193c5ef98f8bf643f8bb3bdd71922ad8
Franklin Wei <frankhwei536@gmail.com>
2014-02-13 16:20:16 -0500

    Rearranged init code

--- kernel_main.c
+++ kernel_main.c
@@ -17,23 +17,33 @@ void dividebyzero(registers_t regs)
 {
   panic("Attempted division by zero!");
 }
-static void early_init(void) // do low-level init
+static inline void early_init(void) // do low-level init
 {
-  init_desc_tables(); // GDT, IDT
-  register_handler(0xD, &null_handler); /// FIXME: int 0xD gets sent constantly
   init_terminal();
   printf("Terminal initialized.\n");
-  init_clock(100);
-  printf("System clock initialized.\n");
-  // init_paging(); // BUGGY!
-}
-static void init(void)
-{
+  init_desc_tables(); // GDT, IDT
+  
+  register_handler(0xD, &null_handler); // for some reason, int 0xD gets sent constantly
+  
   init_ps2();
   printf("Keyboard initialized.\n");
+  
+  init_clock(100); // 100 times per second
+
+  printf("Early init complete.\n");
+  // init_paging(); // BUGGY!
+}
+static inline void init(void)
+{
   // register interrupt handlers
   register_handler(0, &dividebyzero);
-  set_unhandled_panic(true); // we've already registered all the handlers
+  set_unhandled_panic(true); // we've already registered all the interrupt handlers we need
+}
+static int ctrlaltdel_count=0;
+void ctrlaltdel(void)
+{
+  printf("Current tick: %d\n", time());
+  ++ctrlaltdel_count;
 }
 #ifdef __cplusplus
 extern "C"
@@ -47,7 +57,7 @@ int kernel_main(void *mboot_ptr) // kernel entry point
 #ifndef NDEBUG
   printf("Build date: %s, %s\n", build_time, build_date);
 #endif
-  printf("System initialized.\n");
+  printf("System initialized at tick %d.\n", time());
  sys_run:
   goto sys_run; // let the system run
   return 0xDEADBEEF; // we should never get here
