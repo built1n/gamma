cae21e730f9ddf9e66f5fbf12825911d08101733
Franklin Wei <frankhwei536@gmail.com>
2014-02-23 15:03:51 -0500

    Added floppy detection code

--- kernel_main.c
+++ kernel_main.c
@@ -17,24 +17,30 @@ void dividebyzero(registers_t regs)
 {
   panic("Attempted division by zero!");
 }
+void detect_floppy(void)
+{
+  const char* lookup_table[16]={"No drive", "360 KB 5.25\" Drive", "1.2 MB 5.25\" Drive", "720 KB 3.5\" Drive", "1.44 MB 3.5\" Drive", "2.88 MB 3.5\" Drive"};
+  printf("Detecting floppy drives...\n");
+  byte floppy_info=read_cmos(0x10, 0);
+  printf("%s on master.\n", lookup_table[(floppy_info & 0xF0) >> 4]);
+  printf("%s on slave.\n", lookup_table[floppy_info & 0x0F]);
+}
 static inline void early_init(void) // do low-level init
 {
   init_terminal();
   printf("Terminal initialized.\n");
   init_desc_tables(); // GDT, IDT
-  
   register_handler(0xD, &null_handler); // for some reason, int 0xD gets sent constantly
-  
+  register_handler(32, &clock_tick); // sometimes the clock_init function fails to do this for some reason
   init_ps2();
   printf("Keyboard initialized.\n");
-  
-  init_clock(100); // 100 times per second
-
+  init_clock(100); // 100 times per second, seems to freeze sometimes
   printf("Early init complete.\n");
   // init_paging(); // BUGGY!
 }
 static inline void init(void)
 {
+  detect_floppy();
   // register interrupt handlers
   register_handler(0, &dividebyzero);
   set_unhandled_panic(true); // we've already registered all the interrupt handlers we need
@@ -44,6 +50,13 @@ void ctrlaltdel(void)
 {
   printf("Current tick: %d\n", time());
   ++ctrlaltdel_count;
+  if(ctrlaltdel_count==3)
+    {
+      printf("Testing read function: ");
+      char buf[256];
+      read(255, buf);
+      printf("You typed %s\n", buf);
+    }
 }
 #ifdef __cplusplus
 extern "C"
@@ -51,6 +64,7 @@ extern "C"
 int kernel_main(void *mboot_ptr) // kernel entry point
 {
   early_init();
+  printf("Debug: current tick: %d\n");
   printf("Booting...\n");
   init();
   asm volatile("sti"); // enable interrupts
--- util.c
+++ util.c
@@ -10,13 +10,13 @@
 #include "gamma.h"
 #include <stddef.h>
 #include <stdint.h>
-void outb(uint16_t port, uint8_t val)
+void outb(uint16_t port, byte val)
 {
   asm volatile ("outb %1, %0": :"dN" (port), "a" (val));
 }
-uint8_t inb(uint16_t port)
+byte inb(uint16_t port)
 {
-  uint8_t ret;
+  byte ret;
   asm volatile ("inb %1, %0" : "=a" (ret) : "dN" (port));
   return ret;
 }
@@ -25,4 +25,9 @@ uint16_t inw(uint16_t port)
   uint16_t ret;
   asm volatile ("inw %1, %0" : "=a" (ret) : "dN" (port));
   return ret;
+}
+byte read_cmos(byte addr, byte nmi_disable)
+{
+  outb(0x70, ((nmi_disable | 0x1)<<7)|addr);
+  return inb(0x71);
 }
