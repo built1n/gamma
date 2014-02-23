965c860d406376a3e7be8134d14e0a2ab36a234d
Franklin Wei <frankhwei536@gmail.com>
2014-02-23 15:46:05 -0500

    Added upper/lowercase printf hex support

--- term.c
+++ term.c
@@ -210,13 +210,21 @@ void term_putn_bin(uint32_t number)
       number<<=1;
     }
 }
-static char hex_chars[]={'0', '1', '2', '3', '4', '5', '6', '7', '8', '8', 'A', 'B', 'C', 'D', 'E', 'F'};
+static char hex_chars_ucase[]={'0', '1', '2', '3', '4', '5', '6', '7', '8', '8', 'A', 'B', 'C', 'D', 'E', 'F'};
 void term_putn_hex(uint32_t number)
 {
-  term_puts("0x");
   for(int i=0;i<8;++i)
     {
-      term_putchar(hex_chars[((number & 0xF0000000 ) >> 28)]);
+      term_putchar(hex_chars_ucase[((number & 0xF0000000 ) >> 28)]);
+      number<<=4;
+    }
+}
+static char hex_chars_lcase[]={'0', '1', '2', '3', '4', '5', '6', '7', '8', '8', 'a', 'b', 'c', 'd', 'e', 'f'};
+void term_putn_hex_lcase(uint32_t number)
+{
+  for(int i=0;i<8;++i)
+    {
+      term_putchar(hex_chars_lcase[((number & 0xF0000000 ) >> 28)]);
       number<<=4;
     }
 }
@@ -251,8 +259,11 @@ int printf(const char* fmt, ...)
 	    case 'c':
 	      term_putchar(va_arg(va, int));
 	      break;
-	    case 'h':
+	    case 'X':
 	      term_putn_hex(va_arg(va, uint32_t));
+	      break;
+	    case 'x':
+	      term_putn_hex_lcase(va_arg(va, uint32_t));
 	      break;
  	    }
 	}
