929320937ce101cad4ccf1f0f57a39065b87e291
Franklin Wei <frankhwei536@gmail.com>
2014-02-23 15:14:26 -0500

    Added CMOS support

--- util.h
+++ util.h
@@ -7,6 +7,9 @@
    ##     ##  ##   ##  ##   ###   ##  ##   ###   ##  ##   ##
   ########## ##    ## ##    ##    ## ##    ##    ## ##    ##
 */
+#include "gamma.h"
+#include <stdint.h>
 void outb(uint16_t port, uint8_t val);
 uint8_t inb(uint16_t port);
 uint16_t inw(uint16_t port);
+byte read_cmos(byte port, byte nmi_disable); // second controls non-maskable interrupts
