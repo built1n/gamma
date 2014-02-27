eaf46e04e879b57976fb43a0cd046144c7d58025
Franklin Wei <frankhwei536@gmail.com>
2014-02-26 19:49:21 -0500

    fixed printf format string

--- panic.c
+++ panic.c
@@ -11,7 +11,7 @@
 static void idle(){return;}
 void kpanic(const char* str, const char* file, int line)
 {
-  printf("[%d] *KERNEL PANIC* - %s\nAt %s:%d\nSystem halted.\n", time(), str, file, line);
+  printf("*KERNEL PANIC* - %s\nAt %s:%d\nSystem halted.\n", str, file, line);
   asm volatile("cli"); // stop interrupts
   asm volatile("hlt"); // put CPU in a non-executing state
   for(;;)idle(); // loop
