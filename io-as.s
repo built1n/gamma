b2b85b887d2469ba17e0af9711fde6d1a8cc925d
Franklin Wei <frankhwei536@gmail.com>
2014-03-15 19:51:36 -0400

    Rewrote loop function

--- io-as.s
+++ io-as.s
@@ -5,11 +5,9 @@
 loop_while_ptr_zero:
 	pushl %ebp
 	movl %esp, %ebp
-	movl 4(%esp), %ebx # load the address
-	movl $1, %ecx
-loop:	
-	movl (%ebx), %eax # dereference the pointer
-	cmpl %eax, %ecx # compare eax and one
-	jne loop # the pointer is still zero
-	leave
+	movl 4(%esp), %eax
+loop:
+	movl (%eax), %edx
+	testl %edx, %edx
+	je loop
 	ret
